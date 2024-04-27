88f931111df415d70264d67e97532cba85c2ecfe
Ilya Avramchuk <163721512+randredNSTU@users.noreply.github.com>
2024-04-27 17:34:01 +0700

    Add files via upload

--- Ex1.hs
+++ Ex1.hs
@@ -1,11 +1,33 @@
-import Text.Regex.Posix
+module Main where
+
+import Data.List (isPrefixOf)
 
 findSubstrings :: String -> [String]
-findSubstrings s = getAllTextMatches (s =~ "10+1" :: AllTextMatches [] String)
+findSubstrings str = go str
+  where
+    go [] = [] -- базовый случай: если строка пустая, возвращаем пустой список
+    go ('1':'0':xs) = extractSubstring ('1':'0':xs) : go xs -- если встретили "10", извлекаем подстроку и рекурсивно вызываем go с оставшейся частью строки
+    go (_:xs) = go xs -- если не встретили "10", продолжаем поиск в оставшейся части строки
+
+    extractSubstring :: String -> String
+    extractSubstring [] = [] -- базовый случай: если строка пустая, возвращаем пустую строку
+    extractSubstring ('1':'0':xs) = '1':'0':takeWhile (== '0') xs ++ "1" -- извлекаем подстроку между "10" и следующей "1"
+    extractSubstring (_:xs) = extractSubstring xs -- если не встретили "10", продолжаем извлечение подстроки
 
 main :: IO ()
 main = do
-    let str1 = "101tf11flb10001"
-    let str2 = "101001tf11flb10001"
-    putStrLn $ unwords $ findSubstrings str1
-    putStrLn $ unwords $ findSubstrings str2
+  let str1 = "101tf11flb10001" -- Первая строка
+      str2 = "101001tf11flb10001" -- Вторая строка
+      result1 = findSubstrings str1 -- Находим подстроки для первой строки
+      result2 = findSubstrings str2 -- Находим подстроки для второй строки
+
+  putStrLn "Результат для первой строки: " -- Выводим результат для первой строки
+  putStrLn $ formatResult result1 -- Форматируем и выводим результат для первой строки
+
+  putStrLn "Результат для второй строки: " -- Выводим результат для второй строки
+  putStrLn $ formatResult result2 -- Форматируем и выводим результат для второй строки
+
+formatResult :: [String] -> String
+formatResult [] = "."
+formatResult [x] = x ++ "."
+formatResult (x:xs) = x ++ ", " ++ formatResult xs
